e354de5c31b3027551fb9b3c16d7210988842159
RahulBMJ <rsing@bmj.com>
2023-09-21 11:02:12 +0530

    test updated

--- cypress/e2e/collection.feature
+++ cypress/e2e/collection.feature
@@ -1,13 +1,9 @@
-Feature: Check response of BMJopen collections articles
+Feature: Check BMJopen collections articles
 
   Scenario: visit collections page of BMJopen journal 
-    Given I visit browse by collections page 
-    # Then  I visit all the collections
-    Then  I get all the collections
-    # Then I check response of first page articles
-    # Then I check response of remaining page articles
-    #Then I get all the media files with anchor tag on the page
-    #Then I check masking of media file with img tag
-    #Then I check masking of media file with anchor tag
-    #When I get an unmask media file
-    #Then I create a record of unmask media files
+    Given I visit the collection page
+    Then  I get all the collections topic
+    Then  I visit the topics article pages
+    Then  I check the response of articles
+    Then  I check for the missing article data
+    
--- cypress/support/customCommands/Custom.actions.cy.js
+++ cypress/support/customCommands/Custom.actions.cy.js
@@ -1,25 +1,133 @@
-Cypress.Commands.add('LaunchApp',()=>{
-    cy.visit('https://opensource-demo.orangehrmlive.com/')
+const journal = "https://gut.bmj.com";
+
+Cypress.Commands.add("getCollectionPage", () => {
+  cy.visit(`${journal}/pages/browse-by-collection`);
 });
 
-Cypress.Commands.add('InsertUsername',()=>{
-   cy.fixture("elements").then((el)=>{
-    cy.get(el.userField).should('exist').type(el.Username)
-   })
+Cypress.Commands.add("getCollectionTopic", () => {
+  let links = [];
+  const wFile = "cypress/fixtures/collections.json";
+  cy.get('[data-testid="topic-page"] > ul > li').each(($el) => {
+    const href = $el.find("a").attr("href");
+    links.push(href);
+    cy.writeFile(wFile, links);
+  });
 });
 
-Cypress.Commands.add('InsertPassword',()=>{
-    cy.fixture("elements").then((el)=>{
-        cy.get(el.passwordField).should('exist').type(el.Password)
-       })
+Cypress.Commands.add("getTopicArticles", () => {
+  cy.fixture("collections").then((topic) => {
+    for (let i = 0; i < topic.length; i++) {
+      cy.visit({
+        url: `${journal}${topic[i]}`,
+      });
+      cy.getPageCount(topic[i]);
+    }
+  });
 });
 
-Cypress.Commands.add('ClickLogin',()=>{
-    cy.fixture("elements").then((el)=>{
-        cy.get(el.loginButton).should('exist').click()
-       })
+Cypress.Commands.add("getPageCount", (topic) => {
+  cy.getArticleDetails(topic);
+  cy.get('[data-testid="pagination"] > a').each(($ele) => {
+    const next = $ele.text();
+    const page = $ele.attr("href");
+    const url = `${journal}${page}`;
+    if (next != "Next") {
+      cy.visit(url);
+      cy.getArticleDetails(url.replace(`${journal}`, ""));
+    }
+  });
 });
 
-// Cypress.Commands.add('VarifyProduct',()=>{
-//     cy.title().should('include', 'Orange')
-// });
+Cypress.Commands.add("getArticleDetails", (topic) => {
+  let details = [];
+  const wFile = `cypress/downloads/${topic}.json`;
+  cy.get("article").each(($ele) => {
+    const title = $ele.find("a").text();
+    const author = $ele.find("div").text();
+    const date = $ele.find("p").text();
+    const href = $ele.find("a").attr("href");
+    details.push({ title, author, date, href });
+  });
+  cy.writeFile(wFile, details);
+});
+
+Cypress.Commands.add("getArticleResponse", () => {
+    cy.fixture("collections").then((topic) => {
+      for (let i = 0; i < topic.length; i++) {
+        cy.visit({
+          url: `${journal}${topic[i]}`,
+        });
+        cy.getHrefs(topic[i]);
+      }
+    });
+  });
+  
+  Cypress.Commands.add("getHrefs", (topic) => {
+    cy.getResponse(topic);
+    cy.get('[data-testid="pagination"] > a').each(($ele) => {
+      const next = $ele.text();
+      const page = $ele.attr("href");
+      const url = `${journal}${page}`;
+      if (next != "Next") {
+        cy.visit(url);
+        cy.getResponse(url.replace(`${journal}`, ""));
+      }
+    });
+  });
+
+Cypress.Commands.add("getResponse", (topic) => {
+  const links = [];
+  const wFile = `cypress/downloads/Response/${topic}.json`;
+  cy.get("article")
+    .find("a")
+    .each(($a) => {
+      const href = $a.attr("href");
+      cy.request({
+        url: `${href}`,
+        failOnStatusCode: false,
+      }).then((response) => {
+        if (response.status !== 200) {
+          links.push(href);
+          cy.writeFile(wFile, links);
+          cy.log(`${href} GIVES ${response.status}`);
+        }
+      });
+    });
+});
+
+Cypress.Commands.add("getMissingData", () => {
+    cy.fixture("collections").then((topic) => {
+      for (let i = 0; i < topic.length; i++) {
+        cy.visit({
+          url: `${journal}${topic[i]}`,
+        });
+        cy.getPageData(topic[i]);
+      }
+    });
+  });
+  
+  Cypress.Commands.add("getPageData", (topic) => {
+    cy.getArticleData(topic);
+    cy.get('[data-testid="pagination"] > a').each(($ele) => {
+      const next = $ele.text();
+      const page = $ele.attr("href");
+      const url = `${journal}${page}`;
+      if (next != "Next") {
+        cy.visit(url);
+        cy.getArticleData(url.replace(`${journal}`, ""));
+      }
+    });
+  });
+Cypress.Commands.add("getArticleData", (topic) => {
+    let details = [];
+    const wFile = `cypress/downloads/Missed/${topic}.json`;
+    cy.get("article").each(($ele) => {
+      const title = $ele.find("a").text();
+      const author = $ele.find("div").text();
+      const date = $ele.find("p").text();
+      if (!title.length || !author.length || !date.length) {
+        details.push({ title, author, date });
+      }
+    });
+    cy.writeFile(wFile, details);
+  });
